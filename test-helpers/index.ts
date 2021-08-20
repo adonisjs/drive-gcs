b8a49f74df8798798606a028f277790a8e05fcee
Harminder Virk <virk.officials@gmail.com>
2021-08-20 12:54:38 +0530

    ci: fix config for ci

--- test-helpers/index.ts
+++ test-helpers/index.ts
@@ -17,6 +17,7 @@ export const GCS_KEY_FILENAME = process.env.GCS_KEY_FILENAME
 
 export const GCS_KEY = process.env.GCS_KEY
 export const GCS_BUCKET = process.env.GCS_BUCKET!
+export const GCS_PROJECT_ID = process.env.GCS_PROJECT_ID!
 export const GCS_NO_UNIFORM_ACL_BUCKET = process.env.GCS_NO_UNIFORM_ACL_BUCKET!
 
 export const authenticationOptions = GCS_KEY_FILENAME
@@ -24,5 +25,6 @@ export const authenticationOptions = GCS_KEY_FILENAME
       keyFilename: GCS_KEY_FILENAME,
     }
   : {
+      projectId: GCS_PROJECT_ID,
       credentials: JSON.parse(GCS_KEY!),
     }
